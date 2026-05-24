a7d98bd40114586ae1e96c36c502a9db065fa47e
Yavuz Selim Kar <yavuzs.kar@gmail.com>
2026-05-24 15:05:18 +0400

    Create sequential_pairer.py

---
+++ sequential_pairer.py
@@ -0,0 +1,16 @@
+names = []
+
+for i in range(3):
+    name = input("enter your name: ")
+
+    names.append(name)
+
+surnames = []
+
+for i in range(3):
+    surname = input("enter your surname: ")
+
+    surnames.append(surname)
+
+for i in range(3):
+    print(names[i], surnames[i])
